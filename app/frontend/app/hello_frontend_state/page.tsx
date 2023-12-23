27a337c597e81c13136e5364810004b7ba896ecc
smasui-0302 <68088496+smasui-0302@users.noreply.github.com>
2023-12-23 08:06:26 +0000

    feat: add implementation basic on frontend

---
+++ app/frontend/app/hello_frontend_state/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { cpSync } from "fs"
+import { useEffect, useState } from "react"
+
+export default function Page() {
+    const [data, setData] = useState({ name: '初期値' })
+    
+    useEffect(() => {
+        const change = { name: "変更" }
+        setData(change)
+    }, [])
+    
+    return <div>hello {data.name}!</div>
+}
